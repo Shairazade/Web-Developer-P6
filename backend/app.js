1747c22b12200949657531c8ebcb5f196765f91e
Shairazade <119336694+Shairazade@users.noreply.github.com>
2023-04-10 17:59:36 +0200

    edit

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const bodyParser = require('body-parser');
 const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
-const path = require('path');
+const cors = require('cors');
+
 
 const app = express();
 
@@ -16,9 +18,12 @@ mongoose.connect('mongodb+srv://adminuserp6:SHadusocr23@atlascluster.sezgukv.mon
 
 
   app.use(express.json());
+  app.use(bodyParser.json());
+  app.use(cors());
 
 // headers d'accès à l'API
 app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
@@ -27,7 +32,6 @@ app.use((req, res, next) => {
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use(bodyParser.json());
 
 app.use('/api/sauces', saucesRoutes);
 app.use('api/auth', userRoutes);
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -13,7 +13,11 @@ exports.signup = (req, res, next) => {
         });
         user.save()
             .then(() => res.status(201).json({ message: 'Utilisateur crée !'}))
-            .catch(error => res.status(400).json({ error }));
+            .catch(error => {
+                console.log(error);
+                res.status(400).json({ error });
+            });
+
     })
     .catch(error => res.status(500).json({ error }));
 };
