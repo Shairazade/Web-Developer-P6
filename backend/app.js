2abf35be46148e53333aae929afee26b35c98440
Shairazade <119336694+Shairazade@users.noreply.github.com>
2023-04-10 16:21:58 +0200

    add middleware auth + edit app.js + edit controllers + +

--- backend/app.js
+++ backend/app.js
@@ -25,12 +25,10 @@ app.use((req, res, next) => {
     next();
   });
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use(bodyParser.json());
 
-app.use('images', express.static(path.join(__dirname, 'images')));
 app.use('/api/sauces', saucesRoutes);
 app.use('api/auth', userRoutes);
-
-
 module.exports = app;
--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
+
+const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
 const sauceCtrl = require('../controllers/sauces');
 
-router.get('/', sauceCtrl.getAllSauce);
-router.post('/', multer, sauceCtrl.createSauce); 
-router.get('/:id', sauceCtrl.getOneSauce);
-router.put('/:id', sauceCtrl.modifySauce);
-router.delete('/:id', sauceCtrl.deleteSauce);
+router.get('/', auth, sauceCtrl.getAllSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce); 
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
 
 
 
