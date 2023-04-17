fc62caa9a5f70e5c316b63a45d9c36f421b160ad
Shairazade <119336694+Shairazade@users.noreply.github.com>
2023-04-17 15:00:29 +0200

    add / at app.js

--- backend/app.js
+++ backend/app.js
@@ -34,5 +34,5 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 
 
 app.use('/api/sauces', saucesRoutes);
-app.use('api/auth', userRoutes);
+app.use('/api/auth', userRoutes);
 module.exports = app;
